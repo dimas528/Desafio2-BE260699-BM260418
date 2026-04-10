aa11fa2e4c24a534b8dabd23dd944a9092372f89
dimas528 <dimasguardado82@gmail.com>
2026-04-10 11:21:41 -0600

    Creacion del segundo proyecto y ramas de los colaboradores

---
+++ Sistema_de_Registro_de_Notas_Estudiantiles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Registro_de_Notas_Estudiantiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
